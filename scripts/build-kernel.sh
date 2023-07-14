6fb6e28ba9753b117a05e4b447ac5b49052c4a05
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-07-14 02:36:55 +0200

    fake it till you make it

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -9,14 +9,20 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 cd "$(dirname -- "$(readlink -f -- "$0")")" && cd ..
-mkdir -p build && cd build
+#mkdir -p build && cd build
 
-if [ ! -d linux-rockchip ]; then
-    git clone --single-branch --progress -b linux-5.10-gen-rkr4 https://github.com/OpenHD/linux-rockchip.git linux-rockchip
-fi
-cd linux-rockchip
+# if [ ! -d linux-rockchip ]; then
+#     git clone --single-branch --progress -b linux-5.10-gen-rkr4 https://github.com/OpenHD/linux-rockchip.git linux-rockchip
+# fi
 
-# Compile kernel into a deb package
-dpkg-buildpackage -a "$(cat debian/arch)" -d -b -nc -uc
+# cd linux-rockchip
+
+# # Compile kernel into a deb package
+# dpkg-buildpackage -a "$(cat debian/arch)" -d -b -nc -uc
+
+#fake it till you make it
+
+wget https://dl.cloudsmith.io/public/openhd/openhd-2-3-evo/deb/ubuntu/pool/jammy/main/l/li/linux-headers-5.10.160-rockchip_5.10.160-8/linux-headers-5.10.160-rockchip_5.10.160-8_arm64.deb
+wget https://dl.cloudsmith.io/public/openhd/openhd-2-3-evo/deb/ubuntu/pool/jammy/main/l/li/linux-image-5.10.160-rockchip_5.10.160-8/linux-image-5.10.160-rockchip_5.10.160-8_arm64.deb
 
 rm -f ../*.buildinfo ../*.changes
