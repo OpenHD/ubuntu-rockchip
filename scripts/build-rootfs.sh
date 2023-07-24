b78a0588b10b5a6c4f9f1b05af863195ed34584f
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-07-24 21:20:19 +0200

    Update build-rootfs.sh

--- scripts/build-rootfs.sh
+++ scripts/build-rootfs.sh
@@ -240,11 +240,11 @@ mount -t sysfs /sys ${chroot_dir}/sys
 mount -o bind /dev ${chroot_dir}/dev
 mount -o bind /dev/pts ${chroot_dir}/dev/pts
 
-# Install rkaiq
-cp -r ../packages/rkaiq/camera-engine-rkaiq_rk3588_arm64.deb ${chroot_dir}/tmp
-chroot ${chroot_dir} /bin/bash -c "dpkg -i /tmp/camera-engine-rkaiq_rk3588_arm64.deb"
-cp -f ../packages/rkaiq/rkaiq_3A_server ${chroot_dir}/usr/bin
-rm -f ${chroot_dir}/tmp/camera-engine-rkaiq_rk3588_arm64.deb
+# # Install rkaiq
+# cp -r ../packages/rkaiq/camera-engine-rkaiq_rk3588_arm64.deb ${chroot_dir}/tmp
+# chroot ${chroot_dir} /bin/bash -c "dpkg -i /tmp/camera-engine-rkaiq_rk3588_arm64.deb"
+# cp -f ../packages/rkaiq/rkaiq_3A_server ${chroot_dir}/usr/bin
+# rm -f ${chroot_dir}/tmp/camera-engine-rkaiq_rk3588_arm64.deb
 
 # Download and update packages
 cat << EOF | chroot ${chroot_dir} /bin/bash
